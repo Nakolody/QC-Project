99d4fed967ce02fcf0801a84ec965ef32efe2f6b
Nathan Kolody <nakolody@Protonmail.com>
2019-10-01 21:12:33 -0400

    Updated Javascript to add points into objects

---
+++ QC-Project/reagent.js
@@ -0,0 +1,10 @@
+class Reagent {
+    constructor(instrument,lot, analyte,level){
+        this.instrument = instrument;
+        this.lot = lot;
+        this.analyte = analyte;
+        this.level = level;
+        this.date = [];
+        this.value = [];
+    }
+}
