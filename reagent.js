886b09d5d9706cbda0bbd42730616570097b9455
Nathan Kolody <nakolody@Protonmail.com>
2019-10-01 21:07:36 -0400

    Classes modified to contain data points

--- reagent.js
+++ reagent.js
@@ -2,7 +2,7 @@ class Reagent {
     constructor(instrument,lot, analyte, date, value){
         this.instrument = instrument;
         this.lot = lot;
-        this.analyte = analyte;
+        this.analyte = [];
         this.date = date;
         this.value = value;
     }
