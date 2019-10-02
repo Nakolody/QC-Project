525d90e0a323a6ac304d1977e504b39cae7b031d
Nathan Kolody <nakolody@Protonmail.com>
2019-10-01 21:20:03 -0400

    Remove duplicate files

--- reagent.js
+++
@@ -1,9 +0,0 @@
-class Reagent {
-    constructor(instrument,lot, analyte, date, value){
-        this.instrument = instrument;
-        this.lot = lot;
-        this.analyte = [];
-        this.date = date;
-        this.value = value;
-    }
-}
